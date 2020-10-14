5c92c38f113f77a21b76512379d15da2414280b0
amin <aghasempour@gmail.com>
2020-10-14 14:43:14 +0200

    Change loading message

--- R/utilities.R
+++ R/utilities.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname)
-   packageStartupMessage("SDRcausal v 0.2-0")
+   packageStartupMessage("SDRcausal v 0.3.0")
 
 .onUnload <- function(libpath)
     library.dynam.unload("SDRcausal",  libpath)
